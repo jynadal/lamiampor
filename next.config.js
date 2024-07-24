5ea861bb9fc22d22ac860add6271651cd076c127
jynadal <nadaljeanyves@hotmail.fr>
2024-07-25 00:55:58 +0200

    before deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ['teal-deliberate-cephalopod-769.mypinata.cloud'],
+    path: 'https://teal-deliberate-cephalopod-769.mypinata.cloud/',
+  },
+};
